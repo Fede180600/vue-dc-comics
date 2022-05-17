906f2e8a1275ba71a26eef83fbc691bd1fa57869
Fede180600 <fede180600@libero.it>
2022-05-17 18:23:32 +0200

    doing footer-top

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,20 +1,58 @@
-<template lang="">
+<template>
     <footer class="footer">
-        <div class="container">
-            <div class="footer-top">
-                footer top
-            </div>
-            <div class="footer-bottom">
-                footer bottom
+
+        <div class="footer_top">
+            <div class="container">
+                <div class="col_text"></div>
+                <div class="col_logo">
+                    <!-- <img src="../assets/img/dc-logo-bg.png" alt=""> -->
+                </div>
             </div>
         </div>
+        <div class="footer_bottom">
+            <div class="container">
+                footer bottom 
+            </div>
+        </div>
+
     </footer>
 </template>
 <script>
 export default {
-    
+    name: "AppFooter"
+
 }
 </script>
 <style lang="scss" scoped >
-    
+@import "../style/variables.scss";
+
+.footer {
+    height: 470px;
+    background-color: $brand-secondary-color;
+
+    &_top {
+        height: 400px;
+        background-image: url(../assets/img/footer-bg.jpg);
+        background-repeat: no-repeat;
+        background-size: cover;
+        .container {
+            display: flex;
+            justify-content: space-between;
+        }
+        .col_text {
+            width: 40%;
+        }
+        .col_logo {
+            width: 40%;
+            height: 400px;
+            background-image: url(../assets/img/dc-logo-bg.png);
+            background-position: center;
+            background-size: cover;
+        }
+    }
+
+    &_bottom {
+        height: 70px;
+    }
+}
 </style>
